3ea969c33380af25143845f5044423a58a0d0a55
ashleysheng <yeer.sheng@gmail.com>
2016-06-22 11:59:38 -0400

    added conversion functions

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -8,14 +8,24 @@ class coordPoint:
 
 
 def aStar(start,goal):
+    closedSet = []
+    openSet = []
+    openSet.append(start)
+    cameFrom = []
+
 
 
 
     pass
 
+'''***********************************************************************'''
+def coordPointToPointID(coordPoint):
+    return coordPoint.x * 100 + coordPoint.y
 
-
-
+def pointIDToCoordPoint(pointID):
+    resultPoint = coordPoint(int(pointID/100), pointid % 100)
+    return 
+'''***********************************************************************'''
 
 
 
@@ -25,9 +35,9 @@ map_data = genfromtxt('map_data.csv', delimiter=',')
 startPoint = coordPoint(0,0)
 goalPoint = coordPoint(99,99)
 
-print(map_data[startPoint.x][startPoint.y])
+aStar(coordPointToPointID(startPoint),coordPointToPointID(goalPoint))
 
-aStar(3,2)
+# print(map_data[startPoint.x][startPoint.y])
 
 
 
