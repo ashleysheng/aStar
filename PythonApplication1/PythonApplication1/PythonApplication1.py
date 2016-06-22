49b49bc81c812567b8f33f321fee7b9973e382ed
ashleysheng <yeer.sheng@gmail.com>
2016-06-22 16:36:59 -0400

    a* function

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,7 +1,9 @@
 from numpy import genfromtxt
 import numpy as np
 import math
-np.set_printoptions(threshold=np.nan)
+import operator
+xDimension = 100
+yDimension = 100
 
 class coordPoint:
     def __init__(self, x, y):
@@ -9,49 +11,174 @@ class coordPoint:
         self.y = y
 
 
-def heuristic_cost(start,goal):
-    return 0 
-
-
-def aStar(start,goal,mapsize):
-    
-    closedSet = []
-    openSet = []
-    heappush(openSet,start)
-    cameFrom = []
-    dist= []
-    dist[start] = 0
-    fscore = []
-    
-    for i in mapsize:
-        dist.append[math.inf]
-    for i in mapsize:
-        fscore.append[math.inf]
-    
-    dist[start] = 0
-
-
-    
-    
-
-    
-    
-        
-    
-
-
-
-
-    pass
-
 '''***********************************************************************'''
 def coordPointToPointID(coordPoint):
-    return coordPoint.x * 100 + coordPoint.y
+    return coordPoint.x * yDimension + coordPoint.y
 
 def pointIDToCoordPoint(pointID):
-    resultPoint = coordPoint(int(pointID/100), pointid % 100)
-    return 
+    resultPoint = coordPoint(int(pointID/yDimension), pointID % yDimension)
+    return resultPoint
 '''***********************************************************************'''
+
+
+
+def heuristic_cost(start,goal):
+    startPoint = pointIDToCoordPoint(start)
+    goalPoint = pointIDToCoordPoint(goal)
+    xDiff = math.fabs(startPoint.x-goalPoint.x)
+    yDiff = math.fabs(startPoint.y-goalPoint.y)
+    result = math.fabs(xDiff - yDiff) + min(xDiff,yDiff) * 1.414
+    return result 
+
+def getAdjacentNeighbours(currentID,map_data):
+
+    mapsize = xDimension * yDimension
+    left = currentID - yDimension # IDs
+    right = currentID + yDimension
+    top = currentID - 1
+    bottom = currentID + 1
+    currentX = pointIDToCoordPoint(currentID).x
+    currentY = pointIDToCoordPoint(currentID).y
+    
+    if currentX == 0 and currentY == 0:                             # top left corner
+        potentialNeightbours = [right, bottom]
+    elif currentX == 0 and currentY == yDimension - 1:              # bottom left corner
+        potentialNeightbours = [right, top]
+    elif currentX == xDimension - 1 and currentY == 0:              # top right corner
+        potentialNeightbours = [left, bottom]
+    elif currentX == xDimension - 1 and currentY == yDimension - 1: # bottom right corner
+        potentialNeightbours = [left, top]
+    elif currentX == 0:                                             # left edge
+        potentialNeightbours = [right, top, bottom]
+    elif currentX == xDimension - 1:                                # right edge
+        potentialNeightbours = [left, top, bottom]
+    elif currentY == 0:                                             # top edge
+        potentialNeightbours = [left, right, bottom]
+    elif currentY == yDimension - 1:                                # bottom edge
+        potentialNeightbours = [left, right, top]
+    else:
+        potentialNeightbours = [left, right, top, bottom]
+    
+    adjacentNeighbours = []
+    for each in potentialNeightbours:
+        if each >= 0 and each < mapsize:
+            coords = pointIDToCoordPoint(each)
+            if map_data[coords.x][coords.y] == 0:
+                adjacentNeighbours.append(each)
+    return adjacentNeighbours
+
+def getCornerNeighbours(currentID,map_data):
+    mapsize = xDimension * yDimension
+    topLeft = currentID - yDimension - 1
+    bottomLeft = currentID - yDimension + 1
+    topRight = currentID + yDimension - 1
+    bottomRight = currentID + yDimension + 1
+    currentX = pointIDToCoordPoint(currentID).x
+    currentY = pointIDToCoordPoint(currentID).y
+
+    if currentX == 0 and currentY == 0:                             # top left corner
+        potentialNeightbours = [bottomRight]
+    elif currentX == 0 and currentY == yDimension - 1:              # bottom left corner
+        potentialNeightbours = [topRight]
+    elif currentX == xDimension - 1 and currentY == 0:              # top right corner
+        potentialNeightbours = [bottomLeft]
+    elif currentX == xDimension - 1 and currentY == yDimension - 1: # bottom right corner
+        potentialNeightbours = [topLeft]
+    elif currentX == 0:                                             # left edge
+        potentialNeightbours = [topRight, bottomRight]
+    elif currentX == xDimension - 1:                                # right edge
+        potentialNeightbours = [topLeft, bottomLeft]
+    elif currentY == 0:                                             # top edge
+        potentialNeightbours = [bottomLeft, bottomRight]
+    elif currentY == yDimension - 1:                                # bottom edge
+        potentialNeightbours = [topLeft, topRight]
+    else:
+        potentialNeightbours = [topLeft, bottomLeft, topRight, bottomRight]
+    
+    cornerNeighbours = []
+    for each in potentialNeightbours:
+        if each >= 0 and each < mapsize:
+            coords = pointIDToCoordPoint(each)
+            if map_data[coords.x][coords.y] == 0:
+                cornerNeighbours.append(each)
+    return cornerNeighbours
+
+def reconstruct_path(cameFrom, currentID):
+    total_path = [currentID]
+    while currentID in cameFrom.keys():
+        currentID = cameFrom[currentID]
+        total_path.append(currentID)
+    return total_path
+
+
+
+def aStar(startID,goalID,map_data):
+    
+    closedSet = []
+    openSet = []  # IDs
+    openSet.append(startID)
+    cameFrom = {}
+    gScore = {}   # ID --> score
+    fScore = {}
+    mapsize = xDimension * yDimension
+    for i in range (0, mapsize - 1):
+        gScore[i] = 99999
+        fScore[i] = 99999
+    
+    gScore[startID] = 0
+    fScore[startID] = heuristic_cost(startID,goalID)
+
+    while openSet:
+        tupleFScore = sorted(fScore.items(), key=operator.itemgetter(1)) # sort by fScore low to high
+        count = 0
+        currentID = tupleFScore[count][0] # ID with lowest fScore
+        while currentID not in openSet:
+            count = count + 1
+            currentID = tupleFScore[count][0] # ID with lowest fScore
+
+
+        if currentID == goalID:
+            return reconstruct_path(cameFrom, currentID)
+
+        openSet.remove(currentID)
+        if currentID not in closedSet:
+            closedSet.append(currentID)
+        adjacentNeighbours = getAdjacentNeighbours(currentID,map_data)
+        for eachAdjacentNeighbour in adjacentNeighbours:
+            if eachAdjacentNeighbour in closedSet:
+                continue
+            tentative_gScore = gScore[currentID] + 1
+            if eachAdjacentNeighbour not in openSet:
+                openSet.append(eachAdjacentNeighbour)
+            
+            if tentative_gScore >= gScore[eachAdjacentNeighbour]:
+                continue
+            else:
+                cameFrom[eachAdjacentNeighbour] = currentID
+                gScore[eachAdjacentNeighbour] = tentative_gScore
+                fScore[eachAdjacentNeighbour] = gScore[eachAdjacentNeighbour] + heuristic_cost(eachAdjacentNeighbour,goalID)
+        #    fScore[eachAdjacentNeighbour] = gScore[eachAdjacentNeighbour]
+     
+        
+        cornerNeighbours = getCornerNeighbours(currentID,map_data)
+        for eachCornerNeighbour in cornerNeighbours:
+            if eachCornerNeighbour in closedSet:
+                continue
+            tentative_gScore = gScore[currentID] + 1.414
+            if eachCornerNeighbour not in openSet:
+                openSet.append(eachCornerNeighbour)
+            
+                
+            if tentative_gScore >= gScore[eachCornerNeighbour]:
+                continue
+            else:
+                cameFrom[eachCornerNeighbour] = currentID
+                gScore[eachCornerNeighbour] = tentative_gScore
+                fScore[eachCornerNeighbour] = gScore[eachCornerNeighbour] + heuristic_cost(eachCornerNeighbour,goalID)
+           # fScore[eachCornerNeighbour] = gScore[eachCornerNeighbour]
+
+    return False
+
 
 
 
@@ -60,9 +187,10 @@ map_data = genfromtxt('map_data.csv', delimiter=',')
 startPoint = coordPoint(0,0)
 goalPoint = coordPoint(99,99)
 
-print(map_data[startPoint.x][startPoint.y])
 
-# print(map_data[startPoint.x][startPoint.y])
+#print(pointIDToCoordPoint(100))
+#print(pointIDToCoordPoint(99))
 
+print(aStar(0,8888,map_data))
 
 
