8b5319776a705e2841ef54e21afe4943145367d6
ashleysheng <yeer.sheng@gmail.com>
2016-06-22 11:34:24 -0400

    created coordPoint object

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,22 +1,17 @@
 from numpy import genfromtxt
 
 class coordPoint:
-    def __init__(self, **kwargs):
-        return super().__init__(**kwargs)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 
 def aStar(start,goal):
-    print(start+goal)
+
+
+
     pass
-
-
-
-
-
-
-
-
-
 
 
 
@@ -27,8 +22,11 @@ def aStar(start,goal):
 
 print("This line will be printed.")
 map_data = genfromtxt('map_data.csv', delimiter=',')
+startPoint = coordPoint(0,0)
+goalPoint = coordPoint(99,99)
 
-print(map_data[99][99])
+print(map_data[startPoint.x][startPoint.y])
+
 aStar(3,2)
 
 
